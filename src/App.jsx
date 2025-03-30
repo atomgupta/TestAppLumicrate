7d4a93777fa10c351791d3b0f693e4ec44af859a
Pushpak Gupta <pushpakguptacsc1@gmail.com>
2025-03-30 22:33:33 +0530

    changed background

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="relative h-screen w-screen bg-cover bg-center text-[#292828]" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1636150721221-b88b3c2299be?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')" }}>
+    <div className="relative h-screen w-screen bg-cover bg-center text-[#292828]" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1623150502742-6a849aa94be4?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')" }}>
       <div className="absolute top-1 left-1">
         <img src={logo} alt="Logo" className="auto w-32" />
       </div>
